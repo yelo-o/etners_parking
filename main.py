530c5aedca95e6fde08f7984c7d48321cc789368
yelo-o <flyordig@gmail.com>
2022-12-04 11:02:43 +0900

    커스텀티킨터

---
+++ main.py
@@ -0,0 +1,48 @@
+import tkinter
+import customtkinter
+
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+root.title("ETNERS PARKING")
+root.iconbitmap("./favicon.ico")
+
+us_ID = "2b-704"
+us_PW = "20200801"
+
+# 함수 정의 부분
+def login():
+    print("data saved.")
+    info_get = [entry1.get(),entry2.get()]
+    print(info_get)
+
+def autofill():
+    entry1.configure(textvariable=us_ID).get()
+    entry2.configure(textvariable=us_PW).get()
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login system", font=("Roboto", 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+btn = customtkinter.CTkButton(master=frame, text="AutoFill", command=autofill)
+btn.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
