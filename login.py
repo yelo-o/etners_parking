b7d58af159f54b175b56fa94d3f3f096f2b84e32
yelo-o <flyordig@gmail.com>
2022-12-07 21:16:06 +0900

    changing the method of loading id/pw

--- login.py
+++ login.py
@@ -9,21 +9,27 @@ root.geometry("500x350")
 root.title("ETNERS PARKING")
 root.iconbitmap("./favicon.ico")
 
-uspace = ["2b-704","20200801"]
+# 비밀번호는 보안을 위해 로컬 파일에 저장합니다. (pw.txt 파일 참조)
+f = open("pw.txt", 'r')
+uspaces = f.readlines() # 유스페이스 주차장 관리자 아이디와 비밀번호 List에 저장
+uspaces = [uspace.rstrip('\n') for uspace in uspaces]
+f.close()
+
 
 # 함수 정의 부분
 def login():
     print("data saved.")
     info_get = [entry1.get(),entry2.get()]
-    print(info_get)
-    if info_get == uspace:
+    print("입력한 값은", info_get)
+    print("텍스트 파일에서 불러 온 값은", uspaces)
+    if info_get == uspaces:
         print("로그인합니다.")
     else:
         print("아이디와 비밀번호를 확인해주세요.")
 
 def autofill():
-    entry1.configure(placeholder_text=uspace[0])
-    entry2.configure(placeholder_text=uspace[1], show="*")
+    entry1.configure(placeholder_text=uspaces[0])
+    entry2.configure(placeholder_text=uspaces[1], show="*")
 
 
 frame = customtkinter.CTkFrame(master=root)
