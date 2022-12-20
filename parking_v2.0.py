ae5960b5f8312a8b5c2a56312201f46f93225d3b
flyordig <flyordig@gmail.com>
2022-12-20 17:08:14 +0900

    22.12.20

--- parking_v2.0.py
+++ parking_v2.0.py
@@ -292,7 +292,7 @@ def enrollGrp():
 # GUI
 root = ttk.Window()
 root.iconbitmap("./favicon.ico")
-root.title("주차 자동 등록")
+root.title("주차 자동 정산")
 frame1 = ttk.Frame(root, width=700, height=500, bootstyle = "default") # 1번 프레임 좌측에 배치
 frame1.pack(fill=tk.X, expand=True)
 frame2 = ttk.Frame(root, width=700, height=100)
@@ -307,7 +307,7 @@ carList = Listbox(frame1)
 btn_search = ttk.Button(frame1, text="검색(10초 정도 소요됩니다.)", bootstyle="warning" ,command=search_car)
 btn_select = ttk.Button(frame1, text="선택", bootstyle="warning" ,command=select_car)
 
-sep1.grid(row=0, sticky="ew",pady=5)
+sep1.grid(row=0, column=0, sticky="ew",pady=5)
 myLabel1.grid(row=1, column=0, sticky="nsew",padx=5, pady=5)
 carNumEntry.grid(row=2, column=0, sticky="ns",padx=5, pady=5)
 btn_search.grid(row=3, column=0, sticky="ns",padx=5, pady=5)
@@ -317,19 +317,21 @@ btn_select.grid(row=6, column=0, sticky="ns",padx=5, pady=5)
 
 
 # 프레임1의 두번째 열
+sep2 = ttk.Separator(frame1, bootstyle = "danger")
 lblCar = ttk.Label(frame1, text="                차량번호", bootstyle="inverse-dark")
 btn0 = ttk.Button(frame1, text="10분 안에 나가요", bootstyle="primary" ,command=divMin10)
 btn1 = ttk.Button(frame1, text="30분 안에 나가요", bootstyle="primary" ,command=divMin30)
 btn2 = ttk.Button(frame1, text="1시간 안에 나가요", bootstyle="primary" ,command=divHr1)
 
+sep2.grid(row=0, column=1, sticky="ew",pady=5)
 lblCar.grid(row=2, column=1, sticky="nsew", padx=5, pady=5, ipadx=30, ipady=10)
 btn0.grid(row=4, column=1, sticky="nsew", padx=5, pady=5)
 btn1.grid(row=5, column=1, sticky="ew", padx=5, pady=5)
 btn2.grid(row=6, column=1, sticky="nsew", padx=5, pady=5)
 
 # 프레임2
-sep2 = ttk.Separator(frame2, bootstyle = "info")
-sep2.pack(fill=ttk.X, pady=10)
+sep3 = ttk.Separator(frame2, bootstyle = "info")
+sep3.pack(fill=ttk.X, pady=10)
 btn3 = ttk.Button(frame2, text="단체 등록", bootstyle = SUCCESS, command=enrollGrp)
 btn3.pack(padx=100,pady=20)
 
